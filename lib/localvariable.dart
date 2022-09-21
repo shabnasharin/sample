de25fe13facb85e539fd6d8299f3d7a026a53e1b
local <shabnasharin16@gmail.com>
2022-09-21 11:03:36 +0530

    commit

---
+++ lib/localvariable.dart
@@ -0,0 +1,20 @@
+void sum(int a,int b){     //a and b are arguments,parameters or formal parameters
+  print('sum = ${a+b}');
+}
+void sub(int a,int b){
+  print('sub = ${a-b}');
+}
+void mul(int a,int b){
+  print('mul = ${a*b}');
+  }
+void div(int a,int b){
+  print('div = ${a/b}');
+  }
+  void main(){
+  sum(10,20);
+  sub(100,20);
+  mul(500,5);
+  div(50,5);
+  }
+  //default function   ->returntype functionname(){}
+//parameterised function  ->returntype functionname(){}
